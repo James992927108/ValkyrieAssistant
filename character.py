e4a41d12bc5f4a779b5cd6b3dd29cd9cca93fc94
鄧子奇 <antonyteng@dengziqis-MacBook-Pro.local>
2019-01-25 02:54:52 +0800

    完成狀態和角色配對

--- character.py
+++ character.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from urllib import urlopen
 import pandas as pd
 import status
+import re
 
 
 class character():
@@ -15,45 +16,76 @@ class character():
 # import character
 
 
-def get_character_list():
-    home_url = 'http://jam-capture-vcon-ww.ateamid.com/zh_TW'
 
-    # all char page
-    url = home_url + '/character_list/1.html?filter=#1'
-    page = urlopen(url).read()
+# character_list/1.html?filter=#1
+# character_list/1.html#1
+# get_char_list_soup(index)
+
+def get_char_list_soup(index):
+    char_url = home_url + '/character_list/{}.html?filter=#{}'.format(index,index)
+    page = urlopen(char_url).read()
+    soup = BeautifulSoup(page, 'html.parser')
+    return soup
+
+def get_all_char_href(soup):
+    char_href_list = []
+    # 方便測試先以find
+    all_td = soup.find_all('td')
+    for td in all_td:
+        a = td.find('a')
+        try:
+            href = a.get('href').replace('..', '')
+            char_href_list.append(href)
+        except:
+            print("no href")
+    # 可以作過濾重複
+
+    return char_href_list
+
+def get_char_detail(href):
+            
+    df_data = create_table('detail__data--txt', 'data')
+    df_skill = create_table('detail__skill--txt', 'skill')
+    # 還有能力值未取得
+    
+    return df_data, df_skill
+
+def create_table(class_name, column_name):
+    char_url = home_url + href
+    page = urlopen(char_url).read()
     soup = BeautifulSoup(page, 'html.parser')
 
-    td = soup.find('td')
-    a = td.find('a')
-    href = a.get('href').replace('..', '')
-    # char page
-    url = home_url + href
-    page = urlopen(url).read()
-    soup = BeautifulSoup(page, 'html.parser')
+    dl = soup.find('dl', class_= class_name)
 
-    dl = soup.find('dl', class_='detail__skill--txt')
-    dd = dl.text.replace('\n','')
-    # print dd
-    return dd
+    title_list = [re.sub('\s','',x.text) for i, x in enumerate(dl.find_all('dt'))]
+    data_list = [re.sub('\s','',x.text) for i, x in enumerate(dl.find_all('dd'))]
+    
+    data = {
+        'title': title_list,
+        column_name : data_list
+    }
 
-def find_state(dd):
-    # print dd
+    df = pd.DataFrame(data=data, columns=['title', column_name])
+    return df    
+
+def find_state(df_skill):
+    s = []
     status_list = status.get_status_list()
     for state in status_list['name']:
-        if dd.find(state)>0:
-            print state  
-
-
+        # 第二項為效果
+        if df_skill['skill'][2].find(state)>0:
+            s.append(state)
+    return s  
 
 if __name__ == "__main__":
 
-    dd = get_character_list()
-    value = find_state(dd)
-
-
-# f = open('test1.html','w')
-# all_td = soup.find_all('td')
-# for td in all_td:
-#     print type(td)
-#     # f.write(td)
-# f.close
+    home_url = 'http://jam-capture-vcon-ww.ateamid.com/zh_TW'
+    for index in range(1,4):
+        soup = get_char_list_soup(index)
+        char_href_list = get_all_char_href(soup)
+        for href in char_href_list:
+            df_data, df_skill = get_char_detail(href)
+            states = find_state(df_skill)
+            for s in states:
+                print df_data['data'][1] , s
+ 
